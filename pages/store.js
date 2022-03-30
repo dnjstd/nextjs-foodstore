72fee9106acaac3593c16f57ae4fe5e15c3da29d
dnjstd <dnjstd@gmail.com>
2022-03-30 15:19:55 +0900

    REST API 생성-json server

--- pages/store.js
+++ pages/store.js
@@ -1,3 +1,16 @@
+import { useEffect } from "react";
+import axios from "axios";
+
 export default function Store() {
+  useEffect(() => {
+    axios
+      .get("http://localhost:9000/stores")
+      .then((Response) => {
+        console.log(Response.data);
+      })
+      .catch((Error) => {
+        console.log(Error, "데이터를 못가져옴");
+      });
+  }, []);
   return <div>Store페이지입니다.</div>;
 }
